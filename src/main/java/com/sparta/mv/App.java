7aa74115ba742dccebcbdb881d1614e1a039ba64
MattWVile <matt.vile@yahoo.co.uk>
2022-03-14 13:47:30 +0000

    removed hello world

--- src/main/java/com/sparta/mv/App.java
+++ src/main/java/com/sparta/mv/App.java
@@ -6,8 +6,6 @@ package com.sparta.mv;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
     }
 }
