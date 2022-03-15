54e2f6b50a6d6a51ac207afbc410303a4298b9f7
MattWVile <matt.vile@yahoo.co.uk>
2022-03-15 16:18:10 +0000

    added a method to generate a random array between 1000 and -1000 given the length by the user and displayed the information

--- src/main/java/com/sparta/mv/App.java
+++ src/main/java/com/sparta/mv/App.java
@@ -1,12 +1,32 @@
 package com.sparta.mv;
 
-/**
- * Hello world!
- *
- */
+import java.util.Random;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args ) {
+        System.out.println("1.Bubble sort");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please choose a method to sort using: ");
+        int choice = input.nextInt();
+        System.out.print("Please state an array size: ");
+        int arraySize = input.nextInt();
+        int[] arrayToSort = arrayGenerator(arraySize);
+        System.out.println("Sorting this array: " + arrayToSort.toString() + " Using the " + SortFactory.getSorter(choice)+ " method");
+        int[] sortedArray = SortFactory.getSorter(choice).sort(arrayToSort);
+        System.out.println("Sorted array : " + sortedArray.toString() + "This took: "  );
 
+
+
+    }
+
+    private static int[] arrayGenerator(int arraySize) {
+        int[] arrayToSort = new int[arraySize];
+        for (int i = 0; i < arraySize; i ++){
+            int rand = new Random().nextInt(1000 + 1000) - 1000;
+            arrayToSort[i] = rand;
+        }
+        return arrayToSort;
     }
 }
--- src/main/java/com/sparta/mv/view/ViewManager.java
+++ src/main/java/com/sparta/mv/view/ViewManager.java
@@ -1,4 +1,10 @@
 package com.sparta.mv.view;
 
+import com.sparta.mv.SortFactory;
+import com.sparta.mv.Sorter;
+
 public class ViewManager {
+    public static Sorter sorterPicker(int choice){
+        return SortFactory.getSorter(choice);
+    }
 }
