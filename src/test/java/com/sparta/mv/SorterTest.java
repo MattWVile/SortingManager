412a03b3f6eb02598a5c3a15eea6e6ff3c896577
MattWVile <matt.vile@yahoo.co.uk>
2022-03-16 16:56:49 +0000

    added performance test

--- src/test/java/com/sparta/mv/SorterTest.java
+++ src/test/java/com/sparta/mv/SorterTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SorterTest {
     @Test
     @DisplayName("All Sorter methods should sort")
@@ -18,5 +22,23 @@ public class SorterTest {
             num ++;
             Assertions.assertArrayEquals(out, sorter.sort(in));
         }
+    }
+
+    @Test
+    @DisplayName("Time taken to sort")
+    void timeTakenToSort() {
+        int[] in = {7, 2, 3, 5, 74, 6, 8, 9, 4, 1};
+        int num = 1;
+        int[] times = new int[2];
+        while (num <= 2) {
+            Sorter sorter = SortFactory.getSorter(num, 1);
+            long timeStart = System.nanoTime();
+            sorter.sort(in);
+            long timeStop = System.nanoTime();
+            long timeTaken = timeStop - timeStart;
+            times[num - 1] = (int) timeTaken;
+            num++;
+        }
+        System.out.println(Arrays.toString(times));
     }
 }
